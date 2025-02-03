840a7ebfe6fb1562068efdc59428c1bd404b7e07
umahanov <umahanov@gmail.com>
2025-02-03 12:06:08 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Server is running on :8080...")
+	http.ListenAndServe("0.0.0.0:8080", nil)
+
+}
